3483486df6c3ead06bf3cfa048cc913cb82b16e3
Рогозный Дмитрий Михайлович <k911mipt@gmail.com>
2018-04-16 00:06:53 +0300

    removed build files

--- clope-ts/bin/IDatabaseHandler.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.i = 10;
-//# sourceMappingURL=IDatabaseHandler.js.map
--- clope-ts/bin/db/DatabaseHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class DatabaseHandler {
-    readNextTransaction() {
-        throw new Error("Method not implemented.");
-    }
-}
-exports.DatabaseHandler = DatabaseHandler;
-//# sourceMappingURL=DatabaseHandler.js.map
